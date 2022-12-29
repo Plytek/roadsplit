a4fd5f56c57ddc4888fdc1b314892684dbe9c995
bent1337 <kastorf.bent@googlemail.com>
2022-12-29 21:56:34 +0100

    Neue Reise anlegen Design Activity

---
+++ app/src/main/java/com/example/roadsplit/activities/ReiseErstellenActivity.java
@@ -0,0 +1,21 @@
+package com.example.roadsplit.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.roadsplit.R;
+
+public class ReiseErstellenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reise_erstellen);
+
+    }
+
+
+}
