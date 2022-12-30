5e95f2661e14d3fafc6488d3d429062aedc7c6b5
bent1337 <kastorf.bent@googlemail.com>
2022-12-30 04:37:20 +0100

    error reset files

--- app/src/main/java/com/example/roadsplit/activities/MainActivity.java
+++ app/src/main/java/com/example/roadsplit/activities/MainActivity.java
@@ -107,4 +107,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void testBent(View view){
+        Intent intent = new Intent(this, NeueTestActivity.class);
+        startActivity(intent);
+    }
+
 }
